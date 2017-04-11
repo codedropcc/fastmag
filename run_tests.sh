65473d38466f193bf73eb5bea8f7b1c176f5ca51
Dunkon <dk@codedrop.cc>
2017-04-11 17:59:22 +0600

    Added travis-ci

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-clear && vendor/bin/phpunit --bootstrap vendor/autoload.php tests --coverage-html coverage --whitelist code
+clear && vendor/bin/phpunit --bootstrap vendor/autoload.php tests --coverage-clover coverage.xml --whitelist code
