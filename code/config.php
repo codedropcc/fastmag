5b50191715d2a669088093c23e5d5a926434c018
Dunkon <dk@codedrop.cc>
2017-04-11 17:44:33 +0600

    Implemented getting database credentials from magento.
    Close #2

--- code/config.php
+++ code/config.php
@@ -8,7 +8,28 @@ use Fastmag\AttributeHelper;
 
 return [
     Connection::class => function () {
-        return new Connection('mysql', 'magento', 'magento', 'magento', null);
+        $config = [];
+        if (class_exists(\Mage::class)) {
+            $connectionObj = \Mage::getConfig()->getResourceConnectionConfig("default_setup");
+            $config = [
+                'host' => $connectionObj->host,
+                'dbname' => $connectionObj->dbname,
+                'username' => $connectionObj->usename,
+                'password' => $connectionObj->password,
+                'prefix' => \Mage::getConfig()->getTablePrefix(),
+            ];
+        }
+        else {
+            $config = [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magneto',
+                'prefix' => '',
+            ];
+        }
+
+        return new Connection($config['host'], $config['dbname'], $config['username'], $config['password'], $config['prefix']);
     },
     AttributeHelper::class => function (Connection $conn) {
         return new AttributeHelper($conn);
